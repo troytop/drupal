3ad217ef75f49fdf04d0da5905c7598bbcf64a49
Troy Topnik <troyt@activestate.com>
2013-07-19 15:58:15 -0700

    Some cleanup, and adding mollom submodule.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -2,9 +2,9 @@
 
 /**
  * Minimal Stackato-specific settings.php
- * delete this file to trigger the setup wizard
+ * Delete this file to trigger the setup wizard
+ * Use 'git checkout ...' to get it back
  */
-
  
 if (isset($_SERVER["DATABASE_URL"])) {
   $data = parse_url($_SERVER["DATABASE_URL"]);
@@ -33,13 +33,11 @@ else {
  */
 
 $update_free_access = FALSE;
-
 $drupal_hash_salt = '';
 ini_set('session.gc_probability', 1);
 ini_set('session.gc_divisor', 100);
 ini_set('session.gc_maxlifetime', 200000);
 ini_set('session.cookie_lifetime', 2000000);
-
 $conf['404_fast_paths_exclude'] = '/\/(?:styles)\//';
 $conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
 $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
--- stackato-setup.sh
+++ stackato-setup.sh
@@ -2,16 +2,22 @@
 echo "Setting up the filesystem."
 echo $STACKATO_FILESYSTEM
 
-if ! [ -d $STACKATO_FILESYSTEM/files ]
+if [ -d $STACKATO_FILESYSTEM ]
   then
-    echo "No Drupal files in $STACKATO_FILESYSTEM."
-    echo "Creating directory and symlinking..."
-    mkdir -p $STACKATO_FILESYSTEM/files
-    ln -s $STACKATO_FILESYSTEM/files sites/default/files 
-    echo "Finished moving and symlinking"
+    echo "Found a persistent filesystem"
+    if ! [ -d $STACKATO_FILESYSTEM/files ]
+      then
+        echo "No Drupal files in $STACKATO_FILESYSTEM."
+        echo "Creating directory and symlinking..."
+        mkdir -p $STACKATO_FILESYSTEM/files
+        ln -s $STACKATO_FILESYSTEM/files sites/default/files 
+        echo "Finished moving and symlinking"
+      else
+        echo "Persistent Drupal files found. Symlinking..."
+        ln -s $STACKATO_FILESYSTEM/files sites/default/files 
+        echo "Finished deleting and symlinking"
+    fi
   else
-    echo "Persistent Drupal files found. Symlinking..."
-    ln -s $STACKATO_FILESYSTEM/files sites/default/files 
-    echo "Finished deleting and symlinking"
+    echo "No persistent filesystem found, using ephemeral disk."
+    echo "Warning: user content will be lost on restart."
 fi
-
