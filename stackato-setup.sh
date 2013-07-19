88b81b0da73ba70e948c2a5fbb721ae7a88f59a0
Troy Topnik <troyt@activestate.com>
2013-07-19 16:42:45 -0700

    Copying stackato config to 8.x-based branch.

---
+++ stackato-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -d $STACKATO_FILESYSTEM ]
+  then
+    echo "Found a persistent filesystem at: "
+    echo $STACKATO_FILESYSTEM
+    if ! [ -d $STACKATO_FILESYSTEM/files ]
+      then
+        echo "No Drupal files in $STACKATO_FILESYSTEM."
+        echo "Creating directory and symlinking..."
+        mkdir -p $STACKATO_FILESYSTEM/files
+        ln -s $STACKATO_FILESYSTEM/files sites/default/files 
+        echo "Finished moving and symlinking"
+      else
+        echo "Persistent Drupal files found. Symlinking..."
+        ln -s $STACKATO_FILESYSTEM/files sites/default/files 
+        echo "Finished deleting and symlinking"
+    fi
+  else
+    echo "No persistent filesystem found, using ephemeral disk."
+    echo "Warning: user content will be lost on restart."
+fi
