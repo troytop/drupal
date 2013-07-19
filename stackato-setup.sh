b87dee9d9ff2b65c45694b365228f1e854067965
Troy Topnik <troyt@activestate.com>
2013-07-19 15:24:27 -0700

    Minor corrections and adding config for connecting to existing drupal db.

--- stackato-setup.sh
+++ stackato-setup.sh
@@ -2,23 +2,15 @@
 echo "Setting up the filesystem."
 echo $STACKATO_FILESYSTEM
 
-if ! [ -e $STACKATO_FILESYSTEM/all/themes/README.txt]
+if ! [ -d $STACKATO_FILESYSTEM/files ]
   then
     echo "No Drupal files in $STACKATO_FILESYSTEM."
-    echo "Copying pushed directories and symlinking..."
-    mkdir -p $STACKATO_FILESYSTEM/all $STACKATO_FILESYSTEM/files
-    cp -r sites/all/* $STACKATO_FILESYSTEM/all
-    rm -rf sites/all
-    ln -s $STACKATO_FILESYSTEM/all sites/all 
-    cp -r sites/default/files $STACKATO_FILESYSTEM/
-    rm -rf sites/default/files
+    echo "Creating directory and symlinking..."
+    mkdir -p $STACKATO_FILESYSTEM/files
     ln -s $STACKATO_FILESYSTEM/files sites/default/files 
     echo "Finished moving and symlinking"
   else
-    echo "Persistent Drupal files found. Removing ephemeral dirs and symlinking..."
-    rm -rf sites/all
-    ln -s $STACKATO_FILESYSTEM/all sites/all 
-    rm -rf sites/default/files
+    echo "Persistent Drupal files found. Symlinking..."
     ln -s $STACKATO_FILESYSTEM/files sites/default/files 
     echo "Finished deleting and symlinking"
 fi
